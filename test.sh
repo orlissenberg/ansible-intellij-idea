e1994effd9ee31f3b37a6907cc87990ef2464ca0
Onno Lissenberg <orlissenberg@gmail.com>
2015-11-10 22:11:11 +0100

    IDEA 15.0

--- test.sh
+++ test.sh
@@ -13,7 +13,7 @@ EOF
 # Create group_vars for the webservers
 mkdir -p $TMP_DIR/group_vars 2> /dev/null
 cat << EOF > $TMP_DIR/group_vars/webservers
-idea_version: 14.1.4
+idea_version: 15.0
 idea_install_gnome: true
 idea_install_chrome: true
 idea_install_sublime: true
